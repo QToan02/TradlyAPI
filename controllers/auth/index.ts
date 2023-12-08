4bcc8b956889ea614a7df568725c909cd26605d3
Toan <92291488+QToan02@users.noreply.github.com>
2023-12-09 02:16:35 +0700

    Handle login feat and generate token

--- controllers/auth/index.ts
+++ controllers/auth/index.ts
@@ -1,24 +1,51 @@
-import { Request, Response } from 'express'
-import { IUser } from '../../models/user/type'
+import { Response } from 'express'
 import { HydratedDocument } from 'mongoose'
+import jwt from 'jsonwebtoken'
+
+import { IUser, IUserMethods } from '../../models/user/type'
 import User from '../../models/user'
+import { IRequest } from '../../types'
+import { ResponseData } from '../../util'
 
-export const register = async (request: Request, response: Response) => {
-  const { email, firstName, lastName, password, phone, avatar }: IUser = request.body
-
+export const register = async (request: IRequest<IUser>, response: Response) => {
   try {
-    const user: HydratedDocument<IUser> = new User({
-      email,
-      firstName,
-      lastName,
-      password,
-      phone,
-      avatar,
-    })
+    const user: HydratedDocument<IUser> = new User(request.body)
 
     await user.save()
-    response.send(user)
+    ResponseData.withSuccess(response, user)
   } catch (error) {
-    if (error instanceof Error) console.error(error.message)
+    if (error instanceof Error) ResponseData.withError(response, error.message)
   }
+}
+
+export const login = async (
+  request: IRequest<Pick<IUser, 'email' | 'password'>>,
+  response: Response
+) => {
+  try {
+    const { email, password } = request.body
+    const findUser: (IUser & IUserMethods) | null = await User.findOne({ email })
+      .select('password')
+      .exec()
+
+    if (!findUser)
+      return ResponseData.withError(response, 'Wrong credentials, check your email or password')
+
+    const matchPassword: boolean = await findUser.matchPassword(password)
+
+    if (!matchPassword)
+      return ResponseData.withError(response, 'Wrong credentials, check your email or password')
+
+    const token = generateToken(findUser)
+
+    ResponseData.withSuccess(response, token)
+  } catch (error) {
+    if (error instanceof Error) ResponseData.withError(response, error.message)
+  }
+}
+
+export const generateToken = (payload: object | string | Buffer): { token: string } => {
+  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRE })
+
+  return { token }
 }
--- routes/auth/index.ts
+++ routes/auth/index.ts
@@ -1,8 +1,9 @@
 import express from 'express'
-import { register } from '../../controllers/auth'
+import { login, register } from '../../controllers/auth'
 
 const router = express.Router()
 
 router.route('/register').post(register)
+router.route('/login').post(login)
 
 module.exports = router
