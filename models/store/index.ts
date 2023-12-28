8acbc29321a5633255280cce5f845ccbe248ffce
Toan <92291488+QToan02@users.noreply.github.com>
2023-12-28 23:27:54 +0700

    Add more property for Address model

--- models/store/index.ts
+++ models/store/index.ts
@@ -4,7 +4,22 @@ import { IStore } from './type'
 const storeSchema = new Schema<IStore>(
   {
     name: { type: String, required: true },
-    avatar: { type: String, required: true },
+    avatar: {
+      type: String,
+      default:
+        'https://www.cdc.gov/foodsafety/images/comms/features/GettyImages-1247930626-500px.jpg?_=00453',
+    },
+    description: {
+      type: String,
+      default:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In augue nunc vel rhoncus, sed. Neque hendrerit risus ut metus ultrices ac. Dui morbi eu amet id mauris. Eget at ut.',
+    },
+    type: String,
+    address: String,
+    city: String,
+    state: String,
+    country: String,
+    courierName: String,
     user: { type: Types.ObjectId, ref: 'User' },
   },
   { timestamps: true }
--- models/store/type.ts
+++ models/store/type.ts
@@ -4,5 +4,12 @@ import { IUser } from '../user/type'
 export interface IStore extends Document {
   name: string
   avatar: string
+  description: string
+  type: string
+  address: string
+  city: string
+  state: string
+  country: string
+  courierName: string
   user: IUser
 }
